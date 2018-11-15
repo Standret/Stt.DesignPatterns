a66eebcd75660ffab05a5c36e932976bc468eb60
Standret <pstandret@gmail.com>
2018-11-15 23:13:10 +0200

    Add project files.

---
+++ Stt.DesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stt.DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
